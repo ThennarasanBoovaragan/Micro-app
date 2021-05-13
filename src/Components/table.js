69780d78f423c93cb49e3c603b36233b905c1f5b
ThennarasanBoovaragan <81062390+ThennarasanBoovaragan@users.noreply.github.com>
2021-05-13 11:58:29 +0530

    UI Updates

--- src/Components/table.js
+++ src/Components/table.js
@@ -37,28 +37,61 @@ export default function Table(data) {
 
   return (
     <>
-      <div className="table">
-        <table class="table">
-          <thead class="thead-dark">
-            <tr>
-              <th scope="col">Author</th>
-              <th scope="col">Title</th>
-              <th scope="col">Creation date.</th>
-            </tr>
-          </thead>
-          <tbody>
-            {tableData &&
-              tableData.items.map((listValue, index) => {
-                return (
-                  <tr key={index} onClick={() => handleClick(listValue)}>
-                    <td>{listValue.owner.display_name}</td>
-                    <td>{listValue.title}</td>
-                    <td>{dateAndTime(listValue.creation_date)}</td>
+
+      <div class="row">
+        <div class="panel panel-info">
+          <div class="panel-heading">
+            <h3 class="panel-title">Micro Application</h3>
+            <button
+              type="button"
+              class="btn btn-info pull-right"
+              data-toggle="modal"
+              data-target="#myModal"
+            >
+              <i class="glyphicon glyphicon-plus"></i>
+            </button>
+          </div>
+          <div class="panel-body">
+            <div class="table-responsive">
+              <table class="table table-hover">
+                <thead>
+                  <tr>
+                    <th scope="col">Author</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Creation date</th>
+                    <th>Action</th>
                   </tr>
-                );
-              })}
-          </tbody>
-        </table>
+                </thead>
+
+                <tbody>
+                  {tableData &&
+                    tableData.items.map((listValue, index) => {
+                      return (
+                        <tr key={index} onClick={() => handleClick(listValue)}>
+                          <td>{listValue.owner.display_name}</td>
+                          <td>{listValue.title}</td>
+                          <td>{dateAndTime(listValue.creation_date)}</td>
+                          <td>
+                            <button
+                              type="buton"
+                              class="btn btn-info"
+                              data-toggle="modal"
+                              data-target="#myModalu"
+                            >
+                              <i class="glyphicon glyphicon-pencil"></i>
+                            </button>
+                            <button type="buton" class="btn btn-danger">
+                              <i class="glyphicon glyphicon-trash"></i>
+                            </button>
+                          </td>
+                        </tr>
+                      );
+                    })}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
       </div>
       <Modal show={show} onHide={handleClose}>
         <Modal.Header>
